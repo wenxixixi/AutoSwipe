e942640547039a41f0bc2b9e2b59418d56135f11
wenxixixi <252480617@qq.com>
2026-05-25 11:20:50 +0800

    Update MainActivity.java

--- app/src/main/java/com/autoswipe/MainActivity.java
+++ app/src/main/java/com/autoswipe/MainActivity.java
@@ -8,6 +8,7 @@ import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
